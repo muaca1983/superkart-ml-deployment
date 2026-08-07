3df30b69f9a875ed992474acad22edb71b00aa3a
muaca1983 <muaca1983@gmail.com>
2026-08-07 12:54:37 +0200

    Add files via upload

---
+++ app ().py
@@ -0,0 +1,8 @@
+
+# Paste your entire app.py Flask code inside these triple quotes
+import joblib
+import pandas as pd
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+# ... (rest of your app.py code goes here)
