270ea6a084a7d9ddd24aa21c896e716d52123496
muaca1983 <muaca1983@gmail.com>
2026-08-07 13:12:22 +0200

    Delete app ().py

--- app ().py
+++
@@ -1,8 +0,0 @@
-
-# Paste your entire app.py Flask code inside these triple quotes
-import joblib
-import pandas as pd
-from flask import Flask, request, jsonify
-
-app = Flask(__name__)
-# ... (rest of your app.py code goes here)
